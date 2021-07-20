a8c6af5960655f686f10a5268b6f412baedbc33b
Anthony Hugo Almanza <anthonya771@gmail.com>
2021-07-20 13:19:49 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import style from "./assets/style.css";
+export default function App() {
+  return (
+    <div className={style}>
+      <h1>Boiler Plate is working</h1>
+    </div>
+  );
+}
