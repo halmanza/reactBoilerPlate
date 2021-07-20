27639337b9d40f5099ae24e2ed6dc99f177868c5
Anthony Hugo Almanza <anthonya771@gmail.com>
2021-07-20 15:00:22 -0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import style from "./assets/style.css";
 export default function App() {
   return (
     <div className={style}>
-      <h1>Boiler Plate is working</h1>
+      <h1>Boiler Plate </h1>
     </div>
   );
 }
